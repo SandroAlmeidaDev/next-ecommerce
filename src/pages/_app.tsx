cce546a65259767641490257b81e48c182bdcbaf
SandroAlmeidaDev <sandroalmeida25@hotmail.com>
2020-11-08 00:02:05 -0300

    Created customizable font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-import GlobalStyle from '../styles/global'
+import GlobalStyle from '../styles/GlobalStyle'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
 
